32bccc582098bdbdcf1b4a4bee9d8b0cfd764791
杨润琦 <runqiyang@gmail.com>
2017-06-29 11:54:23 +0800

    initial commit

---
+++ drqa/utils.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+from collections import Counter
+
+# Modification: remove unused functions and imports.
+# Origin: https://github.com/facebookresearch/ParlAI/tree/master/parlai/agents/drqa
+
+# ------------------------------------------------------------------------------
+# General logging utilities.
+# ------------------------------------------------------------------------------
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value."""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
