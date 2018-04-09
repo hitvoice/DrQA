90f8a34acb9e95d8f8707e9fb862cb4f606b2290
hitvoice <runqiyang@gmail.com>
2018-04-09 11:56:08 +0800

    more robust downloading
    - skip files that are already downloaded
    - check requirements before starting download

--- download.sh
+++ download.sh
@@ -1,16 +1,54 @@
 #!/usr/bin/env bash
 
-# Download SQuAD
-SQUAD_DIR=SQuAD
-mkdir -p $SQUAD_DIR
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $SQUAD_DIR/train-v1.1.json
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_DIR/dev-v1.1.json
+# check if requirements are met
+REQUIRED=(
+    "wget"
+    "unzip"
+    "python3"
+    "pip"
+)
+for ((i=0;i<${#REQUIRED[@]};++i)); do
+    if ! [ -x "$(command -v ${REQUIRED[i]})" ]; then
+        echo 'Error: ${REQUIRED[i]} is not installed.' >&2
+        exit -1
+    fi
+done
 
-# Download GloVe
+# Download SQuAD & GloVe
+SQUAD_DIR=SQuAD
 GLOVE_DIR=glove
+mkdir -p $SQUAD_DIR
 mkdir -p $GLOVE_DIR
-wget http://nlp.stanford.edu/data/glove.840B.300d.zip -O $GLOVE_DIR/glove.840B.300d.zip
-unzip $GLOVE_DIR/glove.840B.300d.zip -d $GLOVE_DIR
+
+URLS=(
+    "https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json"
+    "https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json"
+    "http://nlp.stanford.edu/data/glove.840B.300d.zip"
+)
+FILES=(
+    "$SQUAD_DIR/train-v1.1.json"
+    "$SQUAD_DIR/dev-v1.1.json"
+    "$GLOVE_DIR/glove.840B.300d.zip"
+)
+for ((i=0;i<${#URLS[@]};++i)); do
+    file=${FILES[i]}
+    url=${URLS[i]}
+    if [ -f $file ]; then
+        echo "$file already exists, skipping download."
+    else
+        wget $url -O $file
+        if [ -f $file ]; then
+            echo "$url successfully downloaded."
+        else
+            echo "$url not successfully downloaded."
+            exit -1
+        fi
+        if [ ${file: -4} == ".zip" ]; then
+            unzip $file -d "$(dirname "$file")"
+        fi
+    fi
+done
 
 # Download SpaCy English language models
 python3 -m spacy download en
+
